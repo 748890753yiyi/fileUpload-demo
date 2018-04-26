4bcc838a54333604b190d14f3af9a02f14074aaf
748890753yiyi <748890753@qq.com>
2018-04-26 18:21:05 +0800

    nodejs upload file demo

---
+++ mouploader.js
@@ -0,0 +1,342 @@
+/**
+ * Created by Moyu on 16/9/13.
+ */
+
+var md5;
+if(typeof require === 'function') {
+    md5 = require('js-md5')
+}
+
+
+function MoUploader(ops) {
+    var default_ops = {
+        // chunk Size: byte
+        chunkSize: (1<<20) * 5,
+        // Number: request Number.
+        // Array: files requests.
+        // Boolean: open or close Slice, if false, chunkSize don't work.
+        request: 3,
+        files: [],
+        uploadUrl: '/',
+        // function: get uploaded pos.
+        // arguments: file, md5, index.
+        // need return a promise object which will return uploaded pos.
+        onContinue: null,
+        // if false, md5 will be setted by filename.
+        md5: true,
+        // md5Size: slice file 0 - md5Size for calculate md5
+        md5Size: (1<<20) * 50,
+        // called when before upload.
+        // arguments: file index or -1 (will begin upload)
+        onBeforeUpload: null,
+        // function: uploading progress listener.
+        // *only listen one request.*
+        // arguments: index, chunkIndex, chunksNum, loaded, total.
+        onProgress: null,
+        // function: overall uploading progress listener.
+        // arguments: index, loaded, total
+        onOverAllProgress: null,
+        // function: called when one request is ended.
+        // arguments: index, chunkIndex, chunksNum
+        onLoad: null,
+        // function: called when one request is aborted.
+        // arguments: index, chunkIndex, chunksNum
+        onAbort: null,
+        // function: called when one request happens error.
+        // arguments: index, chunkIndex, chunksNum
+        onError: null
+    }
+    
+    ops = Object.assign(default_ops, ops);
+
+    var files = Object.assign([], [].slice.call(ops.files))
+    var chunkSize = ops.chunkSize;
+    var md5Size = ops.md5Size;
+    var request = ops.request === true ? 1 : ops.request;
+
+    var __onContinue = ops.onContinue || function () {return new Promise(function (resolve) {resolve(0)});}
+    ops.onContinue = function () {
+        var args = [].slice.call(arguments);
+        console.log("onContinue args: ",args);
+        return new Promise(function (resolve) {
+            __onContinue.apply(null, args)
+                .then(function(pos) {
+                    //文件偏移量大小
+                    var bgBytePos = parseInt(pos);
+                    resolve( bgBytePos < 0 ? 0 : bgBytePos)        
+                })
+        })
+    }
+
+    var uploadList = makeUploadProms(files, false);
+
+    function makeUploadProms(files, isMd5Map) {
+        return files.map(function (file, i) {
+            var data = {
+                type: file.type,
+                lastModified: file.lastModified,
+                name: file.name,
+            }
+            return getMD5Hex(!!ops.md5 && typeof md5 === 'function', file, md5Size, isMd5Map)
+                .then(function (md5str) {
+                    data.md5 = md5str;
+                    return ops.onContinue(file, data.md5, i)
+                        .then(function (pos) {
+                            var uploads = [], chunkData, dataList = [];
+                            var bg = isNaN(pos) ? 0 : Number(pos);
+
+                            //console.log("bg: ",bg);
+
+                            var size = file.slice(bg).size;
+
+                            //console.log("size: ",size);
+
+                            file.upSize = size
+                            data.size = size
+
+                            if(request !== false && size/chunkSize > 1) {
+                                data.chunks = Math.ceil( size/chunkSize )//向上取整
+                                files[i].bg = 0
+                                do {
+                                    chunkData = file.slice(bg, bg + chunkSize);
+                                    uploads.push(chunkData);
+                                    dataList.push({
+                                        pos: isNaN(bg) ? 0 : bg,
+                                        size: chunkData.size
+                                    })
+                                    bg += chunkSize;
+                                } while(bg < file.size);
+
+                            } else {
+                                data.pos = bg
+                                chunkData = file.slice(bg)
+                                uploads.push(chunkData);
+                            }
+
+                            return {
+                                data,
+                                dataList: dataList,
+                                uploads: uploads
+                            }
+                        })
+                })
+
+        });
+    }
+
+    function getMD5Hex(has, file, size, isMd5Map) {
+        /*console.log("has: ",has);
+         console.log("file: ",file);
+         console.log("size: ",size);
+         console.log("isMd5Map: ",isMd5Map);*/
+        return new Promise(function (resolve) {
+            if (has) {
+                var md5text;
+                if(isMd5Map && md5Map[file.name+file.size]) {
+                    md5text = md5Map[file.name+file.size]
+                    resolve(md5text)
+                    return;
+                }
+                var fr = new FileReader();
+                fr.readAsDataURL(file.slice(0, size));
+
+                fr.onload = function (e) {
+                    var text = e.target.result;
+                    md5text = md5(text)
+                    md5Map[file.name+file.size] = md5text
+                    resolve(md5text)
+                }
+            } else {
+                resolve(file.name)
+            }
+        })
+
+    }
+    ops.onBeforeUpload && ops.onBeforeUpload(-1)
+
+    uploadStart(uploadList, true)
+
+    function uploadStart(uploadList, triggerBeforeUpload, focusIndex) {
+        Promise.all(uploadList)
+            .then(function (datas) {
+                datas.forEach(function (upload, i) {
+                    i = focusIndex!=null?focusIndex:i
+                    uploadFile(upload, i, triggerBeforeUpload)
+                })
+            })
+    }
+
+    function uploadFile(upload, index, triggerBeforeUpload) {
+        var formData = new FormData();
+        //console.log("ops onBeforeUpload: ",ops.onBeforeUpload);//进度条
+        triggerBeforeUpload && ops.onBeforeUpload && ops.onBeforeUpload(index, formData)
+
+        upload.data &&
+        Object.keys(upload.data).forEach(function (x) {
+            formData.set(x, upload.data[x])
+        })
+
+        if(request !== false) {
+            files[index].bg = 0;
+            if(request instanceof Array) {
+                uploadFileBySlice(upload, request[index], formData, index, upload.data.chunks || 1)
+            } else {
+                uploadFileBySlice(upload, request, formData, index, upload.data.chunks || 1)
+            }
+        } else {
+            uploadFileBySlice(upload, 1, formData, index, upload.data.chunks || 1)
+            // uploadProm(upload.uploads[0], index, -1, 0)
+        }
+    }
+
+    var md5Map = {}
+
+    files.forEach(function (x) {
+        x.bg = 0
+    });
+    //console.log("files: ",files);
+
+    function uploadFileBySlice (upload, request, formData, index, chunksNum) {
+        var __fn = arguments.callee;//自身引用
+
+        var bg = files[index].bg;
+        var uploads = upload.uploads;
+        var data = upload.data;
+        var lists = upload.dataList;
+        if(uploads.length === 0) {
+            return
+        }
+        return uploads.splice(0, request)
+            .map(function (chunk, i) {
+                if(chunksNum > 1) {
+                    formData.set('chunk', bg + i)
+                    formData.set('pos', lists[bg + i].pos)
+                    formData.set('size', lists[bg + i].size)
+                }
+                formData.set('mo-file', chunk, data.name)
+
+                return uploadProm(formData, index, bg + i, chunksNum)
+                    .then(function (val) {
+                        if(xhrList[index] && xhrList[index].isPaused) {
+                            return;
+                        }
+                        if(files[index].bg === 0) {
+                            files[index].bg += request
+                        } else
+                            files[index].bg++;
+                        return __fn(upload, 1, formData, index, chunksNum)
+                    });
+            })
+    }
+
+
+    var xhrList = new Array(ops.files.length)
+
+    function uploadProm(data, index, chunkIndex, chunksNum) {
+
+        return new Promise(function (resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+
+            //上传事件相关
+            xhr.upload.addEventListener('progress', function(e) {
+                if(!e.lengthComputable) {
+                    return;
+                }
+                // resolve({type: 'progress', xhr: this, index: index})
+                xhrList[index].loaded = xhrList[index].loaded || 0
+                xhr.loaded = e.loaded
+                xhr.total = e.total
+                var sums = xhrList[index].reduce(function (p, n) {
+                    p[0] += n.loaded || 0;
+                    p[1] += n.total || 0;
+                    return p;
+                }, [(files[index].size - files[index].upSize), 0])
+                var otherSize = e.total - data.get('mo-file').size
+                if(ops.onOverAllProgress && !xhrList[index].isPaused) {
+                    var total =
+                        Math.max(
+                            xhrList[index].loaded + sums[1] + (files[index].size - files[index].upSize),
+                            files[index].size + chunksNum*otherSize
+                        );// otherSize*chunksNum;
+                    if((sums[0]+xhrList[index].loaded)>total) {
+                        total = sums[0]+xhrList[index].loaded
+                    }
+                    ops.onOverAllProgress(index, sums[0]+xhrList[index].loaded, total, e);
+                }
+                ops.onProgress && ops.onProgress(index, chunkIndex, chunksNum, e.loaded, e.total, e)
+            })
+            xhr.addEventListener('load', function (e) {
+                resolve({type: 'load', xhr: this, index: index})
+                xhrList[index].loaded = xhrList[index].loaded || 0
+                var otherSize = this.loaded - data.get('mo-file').size
+                xhrList[index].loaded += this.loaded + otherSize;
+                xhrList[index][chunkIndex].isLoaded = true
+                // if(xhrList[index].every(function (xhr) {return !!xhr.isAborted})) {
+                //     xhrList[index].isEnded = true;
+                // }
+
+                ops.onLoad && ops.onLoad(index, chunkIndex, chunksNum, e)
+            })
+            xhr.addEventListener('abort', function (e) {
+                resolve({type: 'abort', xhr: this, index: index, data: data})
+                xhrList[index][chunkIndex].isAborted = true
+                ops.onAbort && ops.onAbort(index, chunkIndex, chunksNum, e)
+            })
+            xhr.addEventListener('error', function (e) {
+                resolve({type: 'error', xhr: this, index: index, data: data})
+                xhrList[index][chunkIndex].isAborted = true
+                ops.onError && ops.onError(index, chunkIndex, chunksNum, e)
+            })
+
+            xhr.open('POST', ops.uploadUrl, true);
+            // xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+            xhr.send(data);
+
+            xhrList[index] = xhrList[index] || new Array(chunksNum)
+            xhrList[index][chunkIndex] = xhr
+        })
+    }
+
+
+    return {
+        pause: function (index) {
+            if(index >= 0) {
+                if(xhrList[index]) {
+                    xhrList[index].isPaused = true
+                    xhrList[index].forEach(function (xhr) {
+                        if(xhr) {
+                            xhr.abort()
+                        }
+                    })
+                }
+            } else {
+                var self = this;
+                xhrList.forEach(function (x, i) {
+                    self.pause(i)
+                })
+            }
+        },
+        continue: function (index) {
+            if(index >= 0) {
+                if(xhrList[index].isPaused/* && !xhrList[index].isEnded*/) {
+                    xhrList[index] = [];
+                    uploadStart(makeUploadProms([files[index]], true), false, index)
+                }
+            } else {
+                var self = this;
+                xhrList.forEach(function (x, i) {
+                    self.continue(i)
+                })
+            }
+        }
+    }
+}
+
+if(typeof define === 'function') {
+    define('MoUploader', MoUploader)
+}
+
+if(typeof module !== 'undefined' && module.exports) {
+    module.exports = MoUploader;
+}
---
+++ server.js
@@ -0,0 +1,183 @@
+/**
+ * Created by Moyu on 16/8/29.
+ */
+
+var http = require('http')
+var fs = require('fs')
+var multiparty = require('multiparty')
+var path = require('path')
+var URL = require('url')
+
+var db = require('./db')
+
+var server = http.createServer(function (req, res) {
+
+    console.log(req.url)
+    if(req.url == '/') {
+        fs.readFile('./demo.html', (err, data) => {
+            if(err) {
+                res.end(err.message)
+            } else {
+                res.end(data.toString())
+            }
+        })
+        return;
+    }
+
+    if(req.url.startsWith('/upload')) {
+        /*res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+            // 'Access-Control-Allow-Headers': 'Accept-Ranges, Content-Encoding, Content-Length, Content-Range',
+            'Access-Control-Expose-Headers': 'Accept-Ranges, Content-Encoding, Content-Length, Content-Range'
+        })*/
+        var param = {}
+        var form = makeForm(param)
+        form.on('close', () => {
+            console.log(param)
+            var _md5 = param.md5;
+            delete param.md5;
+            if(_md5) {
+                db.set(_md5, param)
+                db.save();
+            }
+
+            var d = new Date()
+            res.end(JSON.stringify({
+                errmsg: 'success',
+                errno: 0,
+                result: {
+                    fid: d.getTime(),
+                    ctime: `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`,
+                    allow_edit: 0,
+                    filename: param.name.replace(/\.[^\.]*$/, ''),
+                    filetype: 100,
+                    name: 'Moyu'
+                }
+            }))
+        })
+        form.parse(req)
+        return;
+    }
+
+    if(req.url.startsWith('/getFile')) {
+        res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*'
+        })
+        var query = URL.parse(req.url, true).query
+        var _md5 = query.md5;
+
+        var data = db.get(_md5);
+        if(data) {
+            res.end(JSON.stringify({code: 200, pos: parseInt(data.pos) + parseInt(data.size)}))
+        } else {
+            res.end(JSON.stringify({code: 404, pos: 0}))
+        }
+    }
+
+    req.setTimeout(2000);
+
+    var read = fs.createReadStream('.' + req.url)
+    read.pipe(res)
+    read.on('error', err=>console.error)
+}).on('error', (er) => console.error)
+.listen(4040)
+
+server.on('close', () => {
+    console.log('closed');
+    db.save()
+})
+process.on('uncaughtException', function(err) {
+    console.error(err)
+})
+
+process.on('SIGINT', function() {
+    server.close();
+});
+
+var dataMap = {}
+
+function writeBuffer(bf, path, pos) {
+    var fd = fs.openSync(path, 'a+');
+    fs.writeSync(fd, bf, 0, bf.length, Number(pos) || 0)
+    console.log(`write buffer, pos: ${pos}, path: ${path}, length: ${bf.length}`)
+}
+
+function makeForm(param) {
+    //生成multiparty对象，并配置上传目标路径
+    var form = new multiparty.Form({
+        autoFields: true,
+        autoFiles: false,
+    });
+
+    function store(param, chunks) {
+        console.log("param: ",param);
+        param.chunks = param.chunks || 1
+        param.chunk = param.chunk || 0
+        var p = path.join('./upload', param.name)
+        var bf = Buffer.concat(chunks);
+
+        var json = db.get(param.md5);
+        if(json) {
+            json.pos = parseInt(json.pos!=null?json.pos : 0)
+            json.size = parseInt(json.size!=null?json.size : 0)
+        }
+        if(!json || (json.pos+json.size) <= param.pos) {
+            param.pos = parseInt(param.pos) || 0
+            param.size = bf.length
+            db.set(param.md5, param)
+            db.save();
+            writeBuffer(bf, p, param.pos)
+        }
+    }
+    form.on('progress', (bytesReceived, bytesExpected) => {
+         /*console.log("bytesReceived",bytesReceived)
+         console.log("bytesExpected",bytesExpected)
+         console.log(bytesReceived/bytesExpected)*/
+    })
+    form.on('error', (er) => console.error)
+    form.on('part', (part) => {
+        form.on('aborted', () => {
+            console.log('aborted')
+            store(param, chunks)
+        })
+
+        var chunks = []
+        part.on('data', (data) => {
+            if(part.filename) {
+                chunks.push(data)
+            }
+        }).on('end', () => {
+            console.log('end')
+            store(param, chunks)
+
+            /*
+             dataMap[param.md5] = dataMap[param.md5] || {}
+             var obj = dataMap[param.md5]
+             obj[param.chunk] = bf;
+            if(Object.keys(obj).length == param.chunks) {
+                fs.open(p, 'w+', function(err, fd) {
+                    if (err) {
+                        throw 'error opening file: ' + err;
+                    }
+                    var buff = Buffer.concat(Object.keys(obj).map(k=> obj[k]))
+                    console.log(buff)
+                    fs.write(fd, buff, 0, buff.length, null, function(err) {
+                        if (err) throw 'error writing file: ' + err;
+                        fs.close(fd, function() {
+                            console.log('file written');
+                        })
+                    });
+                });
+                delete dataMap[param.md5]
+            }
+            */
+
+        })
+
+    })
+    form.on('field', (name, value)=>{
+        param[name] = value;
+    });
+    return form;
+}
